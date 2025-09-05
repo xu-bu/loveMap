0a2fc1e5cb53ba5f8d98b1787fa52fc27e6dbb3c
Carroll Wen <carroll.wen@iqair.com>
2025-09-05 16:32:41 +0800

    able to see map and marker on both web and app, set some words on map

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <script setup>
-import { GoogleMap, Marker } from 'vue3-google-map'
+import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
 
 const VITE_GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+const VITE_MAP_ID = import.meta.env.VITE_MAP_ID
 const center = { lat: 40.689247, lng: -74.044502 }
+const markerOptions = { position: center, title: 'START OF LOVE MAP' }
+const pinOptions = { background: '#FBBC04' }
 </script>
 
 <template>
   <GoogleMap
-  :api-key="VITE_GOOGLE_MAPS_API_KEY"
-  style="width: 100%; height: 100vh"
-  :center="center"
-  :zoom="15"
+    :api-key="VITE_GOOGLE_MAPS_API_KEY"
+    :mapId="VITE_MAP_ID"
+    style="width: 100%; height: 100vh"
+    :center="center"
+    :zoom="15"
   >
-    <Marker :options="{ position: center }" />
+    <AdvancedMarker :options="markerOptions" :pin-options="pinOptions"/>
+    <AdvancedMarker :options="markerOptions">
+      <template #content>
+        <div style="background: white; color: black; padding: 5px; border-radius: 5px">
+          START OF LOVE MAP
+        </div>
+      </template>
+    </AdvancedMarker>
   </GoogleMap>
 </template>
