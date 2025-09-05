bd5702d67a11f15188d35d99b3cb9a94d8cfb90d
Carroll Wen <carroll.wen@iqair.com>
2025-09-05 17:58:10 +0800

    add info window

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,62 @@
 <script setup>
-import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
-import { ref, onMounted } from 'vue'
+import { GoogleMap, AdvancedMarker,InfoWindow } from "vue3-google-map";
+import { ref, onMounted } from "vue";
 import { Geolocation } from "@capacitor/geolocation";
+import { log } from "./utils/logger.js";
 
-const VITE_GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
-const VITE_MAP_ID = import.meta.env.VITE_MAP_ID
-const pinOptions = { background: '#FBBC04' }
-let center = { lat: 39.9501, lng: 116.4672 };
-const markerOptions = { position: center }
+const VITE_GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+const VITE_MAP_ID = import.meta.env.VITE_MAP_ID;
+let center = { lat: -25.363, lng: 131.044 };
+const centerSydney = { lat: -33.873220, lng: 151.206176 };
+const makerOptionsSydney = { position: centerSydney, title: 'SYDNEY' };
+const centerPerth = { lat: -31.954877, lng: 115.860462 };
+const markerOptionsPerth = { position: centerPerth, title: 'PERTH' };
 
-onMounted(async () => {
-  if (Capacitor.isNativePlatform()) {
-    await Geolocation.requestPermissions();
-  }
+// onMounted(async () => {
+//   if (Capacitor.isNativePlatform()) {
+//     await Geolocation.requestPermissions();
+//   }
 
-  center = await Geolocation.getCurrentPosition();
-})
+//   const CurrentPosition = await Geolocation.getCurrentPosition();
+
+//   log("Current position:", CurrentPosition);
+//   center = {
+//     lat: CurrentPosition.latitude,
+//     lng: CurrentPosition.coordinates.longitude,
+//   };
+//   log("Current position:", center);
+// });
 </script>
 
 <template>
-    <GoogleMap
-    :api-key="VITE_GOOGLE_MAPS_API_KEY"
+  <GoogleMap
+    :apiKey="VITE_GOOGLE_MAPS_API_KEY"
     :mapId="VITE_MAP_ID"
     style="width: 100%; height: 100vh"
     :center="center"
-    :zoom="15"
+    :zoom="3"
   >
-    <AdvancedMarker :options="markerOptions" :pin-options="pinOptions"/>
-    <AdvancedMarker :options="markerOptions">
+    <AdvancedMarker :options="makerOptionsSydney">
+      <InfoWindow>
+        <h1>Sydney</h1>
+        <div>
+          Default AdvancedMarker With Custom InfoWindow
+        </div>
+      </InfoWindow>
+    </AdvancedMarker>
+
+    <AdvancedMarker :options="markerOptionsPerth">
       <template #content>
         <div style="background: white; color: black; padding: 5px; border-radius: 5px">
-          START OF LOVE MAP
+          Perth
         </div>
       </template>
+      <InfoWindow>
+        <h1>Perth</h1>
+        <div>
+          Custom Content AdvancedMarker With Custom InfoWindow
+        </div>
+      </InfoWindow>
     </AdvancedMarker>
   </GoogleMap>
 </template>
