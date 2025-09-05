8d6ad6a02fb55870fb9c8a0d6732bce9d32986b0
Carroll Wen <carroll.wen@iqair.com>
2025-09-05 16:54:10 +0800

    try to set current position

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <script setup>
 import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
+import { ref, onMounted } from 'vue'
+import { Geolocation } from "@capacitor/geolocation";
 
 const VITE_GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 const VITE_MAP_ID = import.meta.env.VITE_MAP_ID
-const center = { lat: 40.689247, lng: -74.044502 }
-const markerOptions = { position: center, title: 'START OF LOVE MAP' }
 const pinOptions = { background: '#FBBC04' }
+let center = { lat: 39.9501, lng: 116.4672 };
+const markerOptions = { position: center }
+
+onMounted(async () => {
+  if (Capacitor.isNativePlatform()) {
+    await Geolocation.requestPermissions();
+  }
+
+  center = await Geolocation.getCurrentPosition();
+})
 </script>
 
 <template>
-  <GoogleMap
+    <GoogleMap
     :api-key="VITE_GOOGLE_MAPS_API_KEY"
     :mapId="VITE_MAP_ID"
     style="width: 100%; height: 100vh"
