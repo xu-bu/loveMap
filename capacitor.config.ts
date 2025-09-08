8ab87a714a90cd3edbe12f80cc4ccc7bb4be2003
Carroll Wen <carroll.wen@iqair.com>
2025-09-08 10:24:52 +0800

    try to get current location

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'loveMap',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    Geolocation: {
+      permissions: ['location']
+    }
+  }
 };
 
 export default config;
